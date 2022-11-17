f30a1bce69e5f9cd5d5d2a4ac08caeba20c6c5fe
Sonny Huynh <sonny.huynh@strongmind.com>
2022-11-17 16:37:53 -0700

    add test for length check

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -2,16 +2,18 @@ require 'password_checker'
 
 RSpec.describe PasswordChecker do
   before do
-    # Do nothing
+    @target = described_class.new
   end
 
   after do
     # Do nothing
   end
 
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+  context 'Normal passwords' do
+    it 'fail if password is less than 7 characters' do
+      password = "123456"
+      result = @target.check_password(password)
+      expect(result).to eq(false)
     end
   end
 end
