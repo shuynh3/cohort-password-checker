515f5780c1656c324fced82b671b28d1e6d4aebe
Sonny Huynh <sonny.huynh@strongmind.com>
2022-11-17 18:04:33 -0700

    Implement normal password strength

--- lib/password_checker.rb
+++ lib/password_checker.rb
@@ -8,28 +8,37 @@ class PasswordChecker
     @admin ? 10 : 7
   end
 
-  def check_password(password)
-    result = check_characters(password) && check_length(password)
-    @admin ? result && check_special(password) : result
+  def check_password?(password)
+    length_requirement = check_length? password
+    letter_requirement = check_letter? password
+    number_requirement = check_number? password
+    special_requirement = check_special? password
+
+    print_strength length_requirement, letter_requirement, number_requirement
+
+    length_requirement && letter_requirement && number_requirement && special_requirement
   end
 
-  def check_characters(password)
-    check_letter(password) && check_number(password)
-  end
-
-  def check_letter(password)
+  def check_letter?(password)
     password.count("a-zA-Z") > 0 ? true : false
   end
 
-  def check_number(password)
+  def check_number?(password)
     password.count("0-9") > 0 ? true : false
   end
 
-  def check_special(password)
+  def check_special?(password)
+    return true unless @admin
     password.match?(/[!@#$%^&*()_+{}\[\]:;'"\/\\?><.,]/)
   end
 
-  def check_length(password)
+  def check_length?(password)
     password.length >= length ? true : false
+  end
+
+  def print_strength(length_requirement, letter_requirement, number_requirement)
+    puts "Does not contain #{length} characters\n" unless length_requirement
+    puts "Does not contain one letter\n" unless letter_requirement
+    puts "Does not contain one number\n" unless number_requirement
   end
 end
--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -15,19 +15,19 @@ RSpec.describe PasswordChecker do
     context 'length check' do
       it 'false if password is less than 7 characters' do
         password = "12345a"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(false)
       end
 
       it 'true if password is == 7 characters' do
         password = "123456a"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(true)
       end
 
       it 'true if password is > 7 characters' do
         password = "1234567a"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(true)
       end
     end
@@ -35,19 +35,19 @@ RSpec.describe PasswordChecker do
     context 'character check' do
       it 'false if password does not contain letter' do
         password = "1234567"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(false)
       end
 
       it 'false if password does not contain number' do
         password = "abcdefghi"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(false)
       end
 
       it 'true if password contains both' do
         password = "1234567abc"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(true)
       end
     end
@@ -56,50 +56,50 @@ RSpec.describe PasswordChecker do
       it 'does not meet length req' do
         password = "12345a"
         expect do
-          @target.check_password(password)
-        end.to output('Does not contain 7 characters').to_stdout
+          @target.check_password?(password)
+        end.to output("Does not contain 7 characters\n").to_stdout
       end
 
       it 'does not meet letter req' do
         password = "1234567"
         expect do
-          @target.check_password(password)
-        end.to output('Does not contain one letter').to_stdout
+          @target.check_password?(password)
+        end.to output("Does not contain one letter\n").to_stdout
       end
 
       it 'does not meet number req' do
         password = "abcdefg"
         expect do
-          @target.check_password(password)
-        end.to output('Does not contain one number').to_stdout
+          @target.check_password?(password)
+        end.to output("Does not contain one number\n").to_stdout
       end
 
       it 'does not meet letter and number req' do
-        password = "abcdefg"
+        password = "()()()()()()()"
         expect do
-          @target.check_password(password)
-        end.to output('Does not contain one letter\nDoes not contain one number').to_stdout
+          @target.check_password?(password)
+        end.to output("Does not contain one letter\nDoes not contain one number\n").to_stdout
       end
 
       it 'does not meet letter and length req' do
         password = "123456"
         expect do
-          @target.check_password(password)
-        end.to output('Does not contain 7 characters\nDoes not contain one letter').to_stdout
+          @target.check_password?(password)
+        end.to output("Does not contain 7 characters\nDoes not contain one letter\n").to_stdout
       end
 
       it 'does not meet number and length req' do
         password = "abcdef"
         expect do
-          @target.check_password(password)
-        end.to output('Does not contain 7 characters\nDoes not contain one number').to_stdout
+          @target.check_password?(password)
+        end.to output("Does not contain 7 characters\nDoes not contain one number\n").to_stdout
       end
 
       it 'does not meet any requirement' do
         password = ""
         expect do
-          @target.check_password(password)
-        end.to output('Does not contain 7 characters\nDoes not contain one letter\nDoes not contain one number').to_stdout
+          @target.check_password?(password)
+        end.to output("Does not contain 7 characters\nDoes not contain one letter\nDoes not contain one number\n").to_stdout
       end
     end
   end
@@ -112,19 +112,19 @@ RSpec.describe PasswordChecker do
     context 'length check' do
       it 'false if password is less than 10 characters' do
         password = "1234567)a"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(false)
       end
 
       it 'true if password is == 10 characters' do
         password = "12345678)a"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(true)
       end
 
       it 'true if password is > 10 characters' do
         password = "123456789)a"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(true)
       end
     end
@@ -132,26 +132,26 @@ RSpec.describe PasswordChecker do
     context 'character check' do
       it 'false if password does not contain letter' do
         password = "123456789)"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(false)
       end
 
       it 'false if password does not contain number' do
         password = "abcdefghi)"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(false)
       end
 
       it 'false if password does not contain special number' do
         password = "abcdefghi1"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(false)
       end
 
       it 'true if password contains letter, number, special character' do
         special = %w{!@#$%^&*()_+{}\[\]:;'"\/\\?><.,}.sample
         password = "12345678a!#{special}"
-        result = @target.check_password(password)
+        result = @target.check_password?(password)
         expect(result).to eq(true)
       end
     end
